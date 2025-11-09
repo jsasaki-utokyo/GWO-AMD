46515e580ee44ebbc54dfb67bb9039cc625b563c
Jun Sasaki <jsasaki.ece@gmail.com>
2025-11-09 18:46:23 +0900

    Initial commit

---
+++ src/gwo_amd/__init__.py
@@ -0,0 +1,13 @@
+"""
+GWO-AMD package
+
+Utilities for downloading, converting, and validating JMA/GWO datasets.
+"""
+
+__all__ = [
+    "jma_weather_downloader",
+    "jma_to_gwo_converter",
+    "mod_class_met",
+    "verify_gwo_conversion",
+    "config",
+]
